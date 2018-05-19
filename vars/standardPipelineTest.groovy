e27f9a62e226e4493d97220913434a7617ea141a
Kunal Namdev <namdev.kunal0@gmail.com>
2018-05-19 10:30:57 +0530

    initial

---
+++ vars/standardPipelineTest.groovy
@@ -0,0 +1,40 @@
+def call(body) {
+  
+  	def config = [:]
+
+    	body.resolveStrategy = Closure.DELEGATE_FIRST
+ 
+   	body.delegate = config
+   
+ 	body()
+
+  
+ 
+	 node {
+	  
+  		// Clean workspace before doing anything
+	
+    		deleteDir()
+
+	
+    	try {
+	      
+  		stage ('Clone') {
+
+	        	checkout scm
+	 
+       		}
+	        
+	  
+  	} 
+	catch (err) {
+	    
+    		currentBuild.result = 'FAILED'
+	 
+       		throw err
+	   
+ 	}
+
+    }
+
+}
